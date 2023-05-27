ff1324ff19698b6ed67ef667028bce055ef674fa
Cezar Lima <cezzarllima76@gmail.com>
2023-05-26 21:46:11 -0300

    first commit

---
+++ src/components/Filter.jsx
@@ -0,0 +1,25 @@
+const Filter = ({filter, setFilter, setSort}) => {
+  return (
+    <div className="filter">
+        <h2>Filtrar:</h2>
+        <div className="filter-options">
+            <div>
+                <p>Status:</p>
+                <select value={filter} onChange={(e) =>setFilter(e.target.value)}>
+                    <option value="all">Todas</option>
+                    <option value="completed">Completas</option>
+                    <option value="incompleted">Incompletas</option>
+                </select>
+            </div>
+            <div>
+                <p>Ordem alfabetica</p>
+                <button onClick={() => setSort("Asc")}>Asc</button>
+                <button onClick={() => setSort("Desc")}>Desc</button>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Filter
